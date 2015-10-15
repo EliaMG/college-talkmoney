a04aec87bcb1e84cd30c63ddd9645a8730aef105
Elia <elgrenier@gmail.com>
2015-10-15 14:33:07 -0700

    got axis to appear

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -17,17 +17,25 @@ $(function() {
 
   function makeChart(data) {
 
-  // var data = [4, 8, 15, 16, 23, 42];
   var width = 420,
-      barHeight = 20;
+      barHeight = 20,
+      height = barHeight * (data.length + 1);
 
   var x = d3.scale.linear()
       .domain([ 0, data[data.length - 1].net_price ])
-      .range([ 0, width ]);
+      .range([ 0, width]);
+
+  var y = d3.scale.ordinal()
+        .domain(d3.range(data.length))
+        .rangeBands([0, height], .2)
+
+  var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
 
   var chart = d3.select(".bar-chart")
       .attr("width", width)
-      .attr("height", barHeight * data.length);
+      .attr("height", height);
 
   var bar = chart.selectAll("g")
       .data(data)
@@ -37,10 +45,16 @@ $(function() {
       .attr("width", function(d) { return x(d.net_price); })
       .attr("height", barHeight - 1);
 
+
   bar.append("text")
-      .attr("x", function(d) { return x(d.net_price) - 3; })
+      .attr("x", function(d) { return x(d.net_price) - 4; })
       .attr("y", barHeight / 2)
       .attr("dy", ".35em")
       .text(function(d) { return d.name; });
+
+  chart.append("g")
+      .attr("class", "axis")
+      .attr("transform", "translate(0,80)")
+      .call(xAxis);
   }
 });
