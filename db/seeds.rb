5653b2c5cb2ea6b91ad3e953cf1de5a1714a981d
Elia <elgrenier@gmail.com>
2015-10-16 14:16:58 -0700

    wip: db structure and seedings

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'csv'
+
+ CSV.foreach("db/seedscleaned.csv", headers: true) do |row|
+
+   School.create(
+     name: row[0],
+     url: row[4],
+     city: row[1],
+     state: row[2],
+     zip: row[3],
+     lat: row[16],
+     long: row[17],
+     control: row[15],
+     years: row[9],
+     distance_only: row[20],
+     net_price_avg: row[23],
+     net_price_low: row[24],
+     net_price_mid: row[25],
+     net_price_high: row[26],
+     loan_avg: row[44],
+     loan_low: row[45],
+     loan_mid: row[46],
+     loan_high: row[47],
+     rpy_rt_avg: row[40],
+     rpy_rt_low: row[41],
+     rpy_rt_mid: row[42],
+     rpy_rt_high: row[43],
+     debt_90: row[49],
+     debt_75: row[50],
+     debt_25: row[51],
+     debt_10: row[52],
+     p_over_25_k: row[53],
+     earn_mean: row[54],
+     earn_med: row[55],
+     earn_low: row[60],
+     earn_mid: row[61],
+     earn_high: row[62]
+   )
+  end
