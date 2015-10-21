43ba68aee8b391a7ce09f2e472dbeb2a9dd6efc3
Elia <elgrenier@gmail.com>
2015-10-20 17:31:28 -0700

    wip: js charts

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,149 +1,150 @@
 
-
-//   $(".school-select").click(function(event) {
-//     var url = "/pricegraph";
-//     dataCall(event, url);
-//   });
-//
-//   function dataCall(event, url) {
-//     event.preventDefault();
-//     $(".bar-chart").empty();
-//     $.ajax(url, {
-//       type: "get",
-//       success: function (data) {
-//       makeChart(data);
-//     }
-//     });
-//   }
-//
-//   function makeChart(data) {
-//
-//   var width = 420,
-//       barHeight = 22,
-//       height = barHeight * (data.length + 2);
-//
-//   var xScale = d3.scale.linear()
-//       .domain([ 0, data[data.length - 1].net_price ])
-//       .range([ 0, width]);
-//
-//   var yScale = d3.scale.ordinal()
-//         .domain(d3.range(data.length))
-//         .rangeRoundBands([0, height], .1)
-//
-//   var xAxis = d3.svg.axis()
-//     .scale(xScale)
-//     .orient("bottom")
-//     .ticks(6);
-//
-//   var chart = d3.select(".bar-chart")
-//       .attr("width", width)
-//       .attr("height", height + data.length * data.length +10);
-//
-//   chart.selectAll("rect")
-//     .data(data)
-//     .enter()
-//     .append("rect")
-//     .attr("x", 0)
-//     .attr("y", function(d, i) {
-//       return (i * yScale.rangeBand());
-//     })
-//     .attr("height", yScale.rangeBand() - 3)
-//     .attr("width", function(d) {
-//       return xScale(d.net_price);
-//     })
-//       // .attr("transform", function(d,i) {
-//       //     return "translate(" + [yScale(i), 0] + ")"
-//       // })
-//     .attr("fill", function(d, i) {
-//       return "rgb(25, " + (100 + i * 30) + ", 35)";
-//     })
-//     .on("mouseover", function(d) {
-//       var matrix = this.getScreenCTM()
-//       .translate(+ this.getAttribute("x"), + this.getAttribute("y"))
-//
-//       d3.select("#tooltip")
-//         .classed("hidden", false)
-//         .style("left", (window.pageXOffset + matrix.e + 30) + "px")
-//         .style("top", (window.pageYOffset + matrix.f - 30) + "px")
-//         .select("#value")
-//         .html("<strong>" + d.name + "</strong> <br/>" +
-//               "Net Price: $" + d.net_price);
-//     })
-//     .on("mouseout", function() {
-//       //Hide the tooltip
-//       d3.select("#tooltip").classed("hidden", true);
-//     });
-//
-//   chart.append("g")
-//     .attr("class", "axis")
-//     .attr("transform", "translate(0, " + (height - 10) + ")")
-//     .call(xAxis);
-//
-//   chart.append("text")      // text label for the x axis
-//     .attr("transform", "translate(" + (width / 2) + " ," + (height + 25) + ")")
-//     .style("text-anchor", "middle")
-//     .style("font-weight", "bold")
-//     .text("Net Price");
-//   }
-// });
 $(function() {
-var svg= d3.select(".bar-chart")
-       .attr("width", 600)
-       .attr("height", 600);
+  $(".school-select").click(function(event) {
+    var url = "/pricegraph";
+    dataCall(event, url);
+  });
 
-var data = ["blue", "red", "green","purple"];
-var rects = d3.select("svg").selectAll("rect")
-   .data(data)
-   .enter().append("rect")
-   .style("fill",function(d) { return  d ; })
-   .attr("y", 410)
-   .attr("id", function(d,i){return "rect"+i;})
-   .attr("x", function(d,i){ return (i+1)*100-40;})
- .attr("width", 80)
-   .attr("height",1)
-   .attr("stroke","black")
-   .attr("stroke-width",3);
-var ellipses = d3.select("svg").selectAll("ellipse")
-   .data(data)
-   .enter().append("ellipse")
-   .style("fill",function(d) { return  d ; })
-   .attr("cy", 410)
- .attr("cx", function(d,i){ return (i+1)*100;})
- .attr("rx", 40)
- .attr("ry", 30)
-   .attr("stroke","black")
-   .attr("stroke-width",3);   ;
-   var rects2 = d3.select("svg").selectAll("rect2")
-   .data(data)
-   .enter().append("rect")
-   .style("fill",function(d) { return  d ; })
-   .attr("y", 410)
-   .attr("id", function(d,i){return "rect2"+i;})
-   .attr("x", function(d,i){ return (i+1)*100-38;})
- .attr("width",76 )
-   .attr("height",1);
+  function dataCall(event, url) {
+    event.preventDefault();
+    $(".bar-chart").empty();
+    $.ajax(url, {
+      type: "get",
+      success: function (data) {
+      makeChart(data);
+    }
+    });
+  }
 
-var ellipses2 = d3.select("svg").selectAll("ellipse1")
-   .data(data)
-   .enter().append("ellipse")
-   .style("fill",function(d) { return  d ; })
-   .attr("cy", 410)
- .attr("cx", function(d,i){ return (i+1)*100;})
- .attr("rx", 40)
- .attr("ry", 30)
-   .attr("class","ell")
-   .attr("stroke","black")
-   .attr("stroke-width",3);
+  function makeChart(data) {
 
-ellipses2.on("mouseover",function(d,i){ d3.select(this).transition().ease("elastic").duration(1000).attr("cy",(i+2)*75);
-        d3.select("#rect"+i).transition().ease("elastic").duration(1000).attr("height",410-(i+2)*75).attr("y",(i+2)*75);
-        d3.select("#rect2"+i).transition().ease("elastic").duration(1000).attr("height",410-(i+2)*75).attr("y",(i+2)*75);
-       d3.select(this).transition().delay(1000+(i+1)*300).duration((i+1)*600).attr("cy",410);
-        d3.select("#rect"+i).transition().delay(1000+(i+1)*300).duration((i+1)*600).attr("height",0).attr("y",410);
-        d3.select("#rect2"+i).transition().delay(1000+(i+1)*300).duration((i+1)*600).attr("height",0).attr("y",410);
-                                     }
+  var width = 420,
+      barHeight = 22,
+      height = barHeight * (data.length + 2);
 
-           );
+  var xScale = d3.scale.linear()
+      .domain([ 0, data[data.length - 1].net_price ])
+      .range([ 0, width]);
+
+  var yScale = d3.scale.ordinal()
+        .domain(d3.range(data.length))
+        .rangeRoundBands([0, height], .1)
+
+  var xAxis = d3.svg.axis()
+    .scale(xScale)
+    .orient("bottom")
+    .ticks(6);
+
+  var chart = d3.select(".bar-chart")
+      .attr("width", width)
+      .attr("height", height + data.length * data.length +10);
+
+  chart.selectAll("rect")
+    .data(data)
+    .enter()
+    .append("rect")
+    .attr("x", 0)
+    .attr("y", function(d, i) {
+      return (i * yScale.rangeBand());
+    })
+    .attr("height", yScale.rangeBand() - 3)
+    .attr("width", function(d) {
+      return xScale(d.net_price);
+    })
+      // .attr("transform", function(d,i) {
+      //     return "translate(" + [yScale(i), 0] + ")"
+      // })
+    .attr("fill", function(d, i) {
+      return "rgb(25, " + (100 + i * 30) + ", 35)";
+    })
+    .on("mouseover", function(d) {
+      var matrix = this.getScreenCTM()
+      .translate(+ this.getAttribute("x"), + this.getAttribute("y"))
+
+      d3.select("#tooltip")
+        .classed("hidden", false)
+        .style("left", (window.pageXOffset + matrix.e + 30) + "px")
+        .style("top", (window.pageYOffset + matrix.f - 30) + "px")
+        .select("#value")
+        .html("<strong>" + d.name + "</strong> <br/>" +
+              "Net Price: $" + d.net_price);
+    })
+    .on("mouseout", function() {
+      //Hide the tooltip
+      d3.select("#tooltip").classed("hidden", true);
+    });
+
+  chart.append("g")
+    .attr("class", "axis")
+    .attr("transform", "translate(0, " + (height - 10) + ")")
+    .call(xAxis);
+
+  chart.append("text")      // text label for the x axis
+    .attr("transform", "translate(" + (width / 2) + " ," + (height + 25) + ")")
+    .style("text-anchor", "middle")
+    .style("font-weight", "bold")
+    .text("Net Price");
+  };
+
+  var svg= d3.select(".cyl-chart")
+    // .append("svg")
+    .attr("width", 600)
+    .attr("height", 600);
+
+  var dataset = ["blue", "red", "green","purple"];
+
+  var rects = d3.select(".cyl-chart").selectAll("rect")
+    .data(dataset)
+    .enter().append("rect")
+    .style("fill",function(d) { return  d ; })
+    .attr("y", 410)
+    .attr("id", function(d,i){return "rect"+i;})
+    .attr("x", function(d,i){ return (i+1)*100-40;})
+    .attr("width", 80)
+    .attr("height",1)
+    .attr("stroke","black")
+    .attr("stroke-width",3);
+
+  var ellipses = d3.select(".cyl-chart").selectAll("ellipse")
+    .data(dataset)
+    .enter().append("ellipse")
+    .style("fill",function(d) { return  d ; })
+    .attr("cy", 410)
+    .attr("cx", function(d,i){ return (i+1)*100;})
+    .attr("rx", 40)
+    .attr("ry", 30)
+    .attr("stroke","black")
+    .attr("stroke-width",3);   ;
+
+  var rects2 = d3.select(".cyl-chart").selectAll("rect2")
+    .data(dataset)
+    .enter().append("rect")
+    .style("fill",function(d) { return  d ; })
+    .attr("y", 410)
+    .attr("id", function(d,i){return "rect2"+i;})
+    .attr("x", function(d,i){ return (i+1)*100-38;})
+    .attr("width",76 )
+    .attr("height",1);
+
+  var ellipses2 = d3.select(".cyl-chart").selectAll("ellipse1")
+    .data(dataset)
+    .enter().append("ellipse")
+    .style("fill",function(d) { return  d ; })
+    .attr("cy", 410)
+    .attr("cx", function(d,i){ return (i+1)*100;})
+    .attr("rx", 40)
+    .attr("ry", 30)
+    .attr("class","ell")
+    .attr("stroke","black")
+    .attr("stroke-width",3);
+
+  ellipses2.on("mouseover",function(d,i){ d3.select(this).transition().ease("elastic").duration(1000).attr("cy",(i+2)*75);
+    d3.select("#rect"+i).transition().ease("elastic").duration(1000).attr("height",410-(i+2)*75).attr("y",(i+2)*75);
+    d3.select("#rect2"+i).transition().ease("elastic").duration(1000).attr("height",410-(i+2)*75).attr("y",(i+2)*75);
+    d3.select(this).transition().delay(1000+(i+1)*300).duration((i+1)*600).attr("cy",410);
+    d3.select("#rect"+i).transition().delay(1000+(i+1)*300).duration((i+1)*600).attr("height",0).attr("y",410);
+    d3.select("#rect2"+i).transition().delay(1000+(i+1)*300).duration((i+1)*600).attr("height",0).attr("y",410);
+  });
 });
 // bar.append("text")
 //   .attr("x", function(d) { return x(d.net_price) - 4; })
@@ -163,7 +164,5 @@ ellipses2.on("mouseover",function(d,i){ d3.select(this).transition().ease("elast
 //   .ease("exp")
 //     .attr("width", 0)
 //     .remove()
-
-
-    // .transition()
-    // .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+// .transition()
+// .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
