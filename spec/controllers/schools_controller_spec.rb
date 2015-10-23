8b26352b14e67835bdb378fb64b7927906cd9450
Elia <elgrenier@gmail.com>
2015-10-23 14:35:13 -0700

    wip: writing model tests

--- spec/controllers/schools_controller_spec.rb
+++ spec/controllers/schools_controller_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
-
+require 'pry'
 RSpec.describe SchoolsController, type: :controller do
 
   describe "search function" do
     before :each do
-      get :search, term: "seattle u", years: 4
+      School.create(name: "Seattle University", state: "WA", net_price_avg: 12345, control: "private non-profit")
+      get :search, term: "seattle u", years: 4, format: :json
     end
 
     it "should be successful" do
@@ -25,4 +26,31 @@ RSpec.describe SchoolsController, type: :controller do
       end
     end
   end
+
+  # describe "pricegraph function" do
+  #   before :each do
+  #     get :pricegraph, {
+  #       "inc": "low",
+  #       "school-ids": "2666,1382,2828"
+  #     }
+  #   end
+  #
+  #   it "should be successful" do
+  #     expect(response).to be_ok
+  #   end
+  #
+  #   it "should return a json response object" do
+  #     expect(response.header['Content-Type']).to include 'application/json'
+  #   end
+  #
+  #   context "the returned json object" do
+  #     it "has the right keys" do
+  #       data = JSON.parse response.body
+  #
+  #       %w(name control net_price).each do |key|
+  #         expect(data.map(&:keys).flatten.uniq).to include key
+  #       end
+  #     end
+  #   end
+  # end
 end
--- spec/models/school_spec.rb
+++ spec/models/school_spec.rb
@@ -1,7 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe School, type: :model do
+  describe "model validations" do
+
+    it "makes a school in the test db" do
+      School.create(name: "Seattle University", state: "WA", net_price_avg: 12345, control: "private non-profit")
+
+      expect(School.first.name).to eq "Seattle University"
+    end
+
+    it "won't make a school without a name" do
+      school = School.create(state: "MT", net_price_avg: 1345, control: "public")
+
+      expect(school).to_not be_valid
+      expect(school.errors.keys).to include(:name)
+    end
+
+    it "won't make schools with the same name" do
+      school = School.create(name: "Montana State University", state: "MT", net_price_avg: 1345, control: "public")
+      school2 = School.create(name: "Montana State University", state: "MT", net_price_avg: 1345, control: "public")
+
+      expect(school2).to_not be_valid
+      expect(school2.errors.keys).to include(:name)
+    end
+
+  end
+
   describe "model methods" do
+    before :each do
+      School.create(name: "Seattle University", state: "WA", net_price_avg: 12345, control: "private non-profit")
+      School.create(name: "Montana State University", state: "MT", net_price_avg: 1345, control: "public")
+    end
+
+    # it "makes a school in the test db" do
+    #   expect(School.first.name).to eq "Seattle University"
+    # end
+    #
+
 
   end
 end
