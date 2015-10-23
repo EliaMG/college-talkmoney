3f1c8a2a9478039f6a1e5e605823cbfc15991dd8
Elia <elgrenier@gmail.com>
2015-10-23 10:11:28 -0700

    finished cyl school methods

--- app/models/school.rb
+++ app/models/school.rb
@@ -24,10 +24,23 @@ class School < ActiveRecord::Base
 
   def self.cyl_prep(inc, schools)
     ids = schools.split(',').map(&:to_i) #"22,33,444" to [22, 33, 444]
-    net_price = "net_price_" + inc #avg to net_price_avg -column name
-    self.cyl_query(ids, net_price)
+    loan = "loan_" + inc #avg to loan_avg -column name
+    earn = "earn_" + inc
+    self.cyl_query(ids, loan, earn)
   end
 
-
-
+  def self.cyl_query(ids, loan, earn)
+    schools = []
+    ids.each do |id|
+      school = School.select("name", "loan_avg", "#{loan} AS loan", "earn_avg", "#{earn} AS earn", "p_over_25_k").where(["id = ?", id]).first
+      if school.loan == 0
+        school.loan = school.loan_avg
+      end
+      if school.earn == 0
+        school.earn = school.earn_avg
+      end
+      schools << school
+    end
+    return schools
+  end
 end
